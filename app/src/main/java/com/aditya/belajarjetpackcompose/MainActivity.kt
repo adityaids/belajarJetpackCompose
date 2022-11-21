6027ca118a3132dc851d061797257b951744c4ac
Aditya <aditya.purnamaaa@gmail.com>
2022-11-21 21:33:37 +0700

    chore(app): init

--- app/src/main/java/com/aditya/belajarjetpackcompose/MainActivity.kt
+++ app/src/main/java/com/aditya/belajarjetpackcompose/MainActivity.kt
@@ -1,8 +1,12 @@
 package com.aditya.belajarjetpackcompose
 
+import android.content.res.Configuration.UI_MODE_NIGHT_YES
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.animation.core.Spring
+import androidx.compose.animation.core.animateDpAsState
+import androidx.compose.animation.core.spring
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
@@ -11,18 +15,25 @@ import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.material.Card
 import androidx.compose.material.Icon
 import androidx.compose.material.IconButton
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
-import androidx.compose.material.Typography
 import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.ExpandLess
 import androidx.compose.material.icons.outlined.ExpandMore
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.layout.VerticalAlignmentLine
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontStyle
 import androidx.compose.ui.text.font.FontWeight
@@ -34,6 +45,18 @@ import androidx.compose.ui.unit.sp
 import com.aditya.belajarjetpackcompose.ui.theme.BelajarJetpackComposeTheme
 
 private val nameList = listOf(
+    "Mas Ibnu",
+    "Adit",
+    "Ferrian",
+    "Iqbal",
+    "Mas Ibnu",
+    "Adit",
+    "Ferrian",
+    "Iqbal",
+    "Mas Ibnu",
+    "Adit",
+    "Ferrian",
+    "Iqbal",
     "Mas Ibnu",
     "Adit",
     "Ferrian",
@@ -56,70 +79,87 @@ fun HelloJetpackComposeApp() {
         modifier = Modifier.fillMaxSize(),
         color = MaterialTheme.colors.background
     ) {
-        GreetingList(names = nameList)
+        GreetingList(nameList)
+    }
+}
+
+@Preview(showBackground = true, device = Devices.PIXEL_4)
+@Preview(showBackground = true, device = Devices.PIXEL_4, uiMode = UI_MODE_NIGHT_YES)
+@Composable
+fun HelloJetpackComposeAppPreview() {
+    BelajarJetpackComposeTheme {
+        HelloJetpackComposeApp()
     }
 }
 
 @Composable
 fun GreetingList(names: List<String>) {
     if (names.isNotEmpty()) {
-        Column() {
-            for (namePerson in names) {
-                Greeting(name = namePerson)
+        LazyColumn {
+            items(names) { name ->
+                Greeting(name)
             }
         }
     } else {
         Box(contentAlignment = Alignment.Center) {
-            Text(
-                text = "No Person To Greet :(",
-                textAlign = TextAlign.Center
-            )
+            Text("No people to great :(")
         }
     }
 }
 
 @Composable
 fun Greeting(name: String) {
-    var isExpanded = false
-    Row(
-        modifier = Modifier.padding(8.dp),
-        verticalAlignment = Alignment.CenterVertically
-    ){
-        Image(
-            painter = painterResource(id = R.drawable.jetpack_compose),
-            contentDescription = "image jetpack",
-            modifier = Modifier.size(80.dp)
+    var isExpanded by remember { mutableStateOf(false) }
+    val animatedSizeDp by animateDpAsState(
+        targetValue = if (isExpanded) 120.dp else 80.dp,
+        animationSpec = spring(
+            dampingRatio = Spring.DampingRatioMediumBouncy,
+            stiffness = Spring.StiffnessLow
         )
-        Spacer(modifier = Modifier.padding(8.dp))
-        Column() {
-            Text(
-                text = "Hello $name!",
-                fontSize = 24.sp,
-                fontWeight = FontWeight.Bold
+    )
+    Card (
+        backgroundColor = MaterialTheme.colors.primary,
+        shape = MaterialTheme.shapes.medium,
+        modifier = Modifier.padding(vertical = 4.dp, horizontal = 8.dp)
+    ) {
+        Row(
+            modifier = Modifier.padding(8.dp),
+            verticalAlignment = Alignment.CenterVertically
+        ) {
+            Image(
+                painter = painterResource(R.drawable.jetpack_compose),
+                contentDescription = "Logo Jetpack Compose",
+                modifier = Modifier.size(animatedSizeDp)
             )
-            Text(
-                text = "Hallo Dicoding"
-            )
-        }
-        IconButton(onClick = { isExpanded = !isExpanded }) {
-            Icon(
-                imageVector = Icons.Outlined.ExpandMore,
-                contentDescription = "Show more"
-            )
+            Spacer(modifier = Modifier.width(8.dp))
+            Column(modifier = Modifier.weight(1f)) {
+                Text(
+                    text = "Hello $name!",
+                    fontSize = 24.sp,
+                    fontWeight = FontWeight.Bold,
+                )
+                Text(
+                    text = "Welcome to Dicoding!",
+                    style = MaterialTheme.typography.body1.copy(
+                        fontStyle = FontStyle.Italic
+                    )
+                )
+            }
+            IconButton(onClick = { isExpanded = !isExpanded }) {
+                Icon(
+                    imageVector = if (isExpanded) Icons.Filled.ExpandLess else Icons.Outlined.ExpandMore,
+                    contentDescription = if (isExpanded) "Show less" else "Show more"
+                )
+            }
         }
     }
 }
 
 @Preview(showBackground = true)
+@Preview(showBackground = true, uiMode = UI_MODE_NIGHT_YES)
 @Composable
 fun DefaultPreview() {
-    BelajarJetpackComposeTheme {
-        Greeting("Android")
+    BelajarJetpackComposeTheme() {
+        Greeting("Jetpack Compose")
     }
-}
-
-@Preview(showBackground = true, device = Devices.PIXEL_4)
-@Composable
-fun BelajarJetpackComposePreview(){
-    HelloJetpackComposeApp()
 }
