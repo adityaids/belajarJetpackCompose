380beaf136d75820434ea3d46f51ee4322bc6727
Aditya <aditya.purnamaaa@gmail.com>
2022-11-22 10:39:14 +0700

    feat(app): new compose learn layouting

---
+++ app/src/main/java/com/aditya/belajarjetpackcompose/BelajarLayoutComposeActivity.kt
@@ -0,0 +1,37 @@
+package com.aditya.belajarjetpackcompose
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.aditya.belajarjetpackcompose.ui.theme.BelajarJetpackComposeTheme
+
+class BelajarLayoutComposeActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            BelajarJetpackComposeTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colors.background
+                ) {
+                }
+            }
+        }
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun DefaultPreview2() {
+    BelajarJetpackComposeTheme {
+
+    }
+}
